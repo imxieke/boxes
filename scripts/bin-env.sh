c51851bbd0ad6b787e7d0246baec59bad6093f82
imxieke <oss@live.hk>
2024-04-17 18:06:15 +0800

    update Linux Env and Proxy

--- scripts/bin-env.sh
+++ scripts/bin-env.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 ###
- # @Author: Cloudflying
- # @Date: 2022-09-02 12:18:39
- # @LastEditTime: 2023-10-30 19:29:55
- # @LastEditors: Cloudflying
- # @Description:
- # @FilePath: /.boxs/scripts/bin-env.sh
+# @Author: Cloudflying
+# @Date: 2022-09-02 12:18:39
+# @LastEditTime: 2024-04-17 11:36:27
+# @LastEditors: Cloudflying
+# @Description:
+# @FilePath: /.boxs/scripts/bin-env.sh
 ###
 
 # 加载自定义目录二进制文件 垃圾的 macOS 还不能删
@@ -14,46 +14,53 @@
 # PATH
 # Boxs PATH
 if [[ -d "${BOXS_HOME}/bin" ]]; then
-	# echo "[+] export PATH ${BOXS_HOME}/bin"
-	echo "[+] Set PATH"
-	export PATH="${BOXS_HOME}/bin:${PATH}"
+    # echo "[+] export PATH ${BOXS_HOME}/bin"
+    echo "[+] Set PATH"
+    export PATH="${BOXS_HOME}/bin:${PATH}"
 fi
 
 if [[ -d "${HOME}/.bin" ]]; then
-	# echo "[+] export PATH ${HOME}/.bin"
-	export PATH="${HOME}/.bin:${PATH}"
+    # echo "[+] export PATH ${HOME}/.bin"
+    export PATH="${HOME}/.bin:${PATH}"
 fi
 
 # Custom
 if [[ -d "/usr/local/opt/whois/bin" ]]; then
-	export PATH="/usr/local/opt/whois/bin:${PATH}"
+    export PATH="/usr/local/opt/whois/bin:${PATH}"
 fi
 
 # 第三方不能通过包管理的软件包
-if [[ -d ~/.local/share/boxs/bin ]];then
-	# echo "[+] export PATH boxs"
-	export PATH="${HOME}/.local/share/boxs/bin:${PATH}"
+if [[ -d ~/.local/share/boxs/bin ]]; then
+    # echo "[+] export PATH boxs"
+    export PATH="${HOME}/.local/share/boxs/bin:${PATH}"
 fi
 
 # 加载自定义版本 PHP
 if [[ -d "/usr/local/opt/php@${PHP_VERSION}/bin" ]]; then
-	echo "[+] Switch PHP Version To: ${PHP_VERSION}"
-	export PATH="/usr/local/opt/php@${PHP_VERSION}/bin:${PATH}"
-	export PATH="/usr/local/opt/php@${PHP_VERSION}/sbin:${PATH}"
-	export LDFLAGS="${LDFLAGS} -L/usr/local/opt/php@${PHP_VERSION}/lib"
-	export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/php@${PHP_VERSION}/include"
+    echo "[+] Switch PHP Version To: ${PHP_VERSION}"
+    export PATH="/usr/local/opt/php@${PHP_VERSION}/bin:${PATH}"
+    export PATH="/usr/local/opt/php@${PHP_VERSION}/sbin:${PATH}"
+    export LDFLAGS="${LDFLAGS} -L/usr/local/opt/php@${PHP_VERSION}/lib"
+    export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/php@${PHP_VERSION}/include"
 fi
 
 # 覆盖系统自带的 python3
 # if [[ -d "/usr/local/opt/python3" ]]; then
-	# echo "[+] export PATH python3"
-    # export PATH="/usr/local/opt/python3/bin:${PATH}"
+# echo "[+] export PATH python3"
+# export PATH="/usr/local/opt/python3/bin:${PATH}"
 # fi
 
 # PHP Composer
-if [[ -d ~/.composer/vendor/bin ]];then
-	# echo "[+] export PATH composer"
-	export PATH="${HOME}/.composer/vendor/bin:${PATH}"
+# for macOS
+if [[ -d ~/.composer/vendor/bin ]]; then
+    # echo "[+] export PATH composer"
+    export PATH="${HOME}/.composer/vendor/bin:${PATH}"
+fi
+
+# for Linux
+if [[ -d ~/.config/composer/vendor/bin ]]; then
+    # echo "[+] export PATH composer"
+    export PATH="${HOME}/.config/composer/vendor/bin:${PATH}"
 fi
 
 # if [[ -d "~/.cargo/bin" ]]; then
@@ -62,49 +69,49 @@ fi
 # fi
 
 if [[ -d "/usr/games" ]]; then
-	# echo "[+] export PATH games"
-	export PATH="${PATH}:/usr/games"
+    # echo "[+] export PATH games"
+    export PATH="${PATH}:/usr/games"
 fi
 
 # 最新版本并覆盖所有其他版本 包括系统自带的
 if [[ -d "/usr/local/opt/python3/bin/" ]]; then
-	# echo "[+] export PATH Python"
-	export PATH="/usr/local/opt/python3/libexec/bin:${PATH}"
+    # echo "[+] export PATH Python"
+    export PATH="/usr/local/opt/python3/libexec/bin:${PATH}"
 fi
 
 if [[ -d "/usr/local/games" ]]; then
-	# echo "[+] export PATH /usr/local/games"
-	export PATH="${PATH}:/usr/local/games"
+    # echo "[+] export PATH /usr/local/games"
+    export PATH="${PATH}:/usr/local/games"
 fi
 
 # if [[ -d "/usr/local/opt/coreutils/bin" ]]; then
-	# echo "[+] export PATH coreutils"
-	# export PATH="/usr/local/opt/coreutils/bin:${PATH}"
+# echo "[+] export PATH coreutils"
+# export PATH="/usr/local/opt/coreutils/bin:${PATH}"
 # fi
 
 if [[ -d "/usr/local/opt/coreutils/libexec/gnubin" ]]; then
-	# echo "[+] export PATH gnu utils"
-	export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+    # echo "[+] export PATH gnu utils"
+    export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
 fi
 
 if [[ -d "/usr/local/opt/coreutils/libexec/gnuman" ]]; then
-	# echo "[+] export PATH gnu man"
-	export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}
+    # echo "[+] export PATH gnu man"
+    export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}
 fi
 
 # if [[ -d "/usr/local/opt/mtr/sbin" ]]; then
-	# echo "[+] export PATH mtr"
-	# export PATH="/usr/local/opt/mtr/sbin:${PATH}"
+# echo "[+] export PATH mtr"
+# export PATH="/usr/local/opt/mtr/sbin:${PATH}"
 # fi
 
 # if [[ -d "/usr/local/opt/universal-ctags/bin" ]]; then
-	# echo "[+] export PATH ctags"
-	# export PATH="/usr/local/opt/universal-ctags/bin:${PATH}"
+# echo "[+] export PATH ctags"
+# export PATH="/usr/local/opt/universal-ctags/bin:${PATH}"
 # fi
 
 # if [[ -d "/usr/local/opt/ctags/bin" ]]; then
-	# echo "[+] export PATH ctags"
-	# export PATH="/usr/local/opt/ctags/bin:${PATH}"
+# echo "[+] export PATH ctags"
+# export PATH="/usr/local/opt/ctags/bin:${PATH}"
 # fi
 
 # load nvm
@@ -117,23 +124,22 @@ fi
 
 # macOS 预置了老版本 需要覆盖
 if [[ -f "/usr/local/opt/ruby/bin/gem" ]]; then
-	export PATH="/usr/local/opt/ruby/bin:${PATH}"
-  	export LDFLAGS="-L/usr/local/opt/ruby/lib ${LDFLAGS}"
-  	export CPPFLAGS="-I/usr/local/opt/ruby/include ${CPPFLAGS}"
-  	# export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig:${PKG_CONFIG_PATH}"
-  	export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+    export PATH="/usr/local/opt/ruby/bin:${PATH}"
+    export LDFLAGS="-L/usr/local/opt/ruby/lib ${LDFLAGS}"
+    export CPPFLAGS="-I/usr/local/opt/ruby/include ${CPPFLAGS}"
+    # export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig:${PKG_CONFIG_PATH}"
+    export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
 fi
 
-
 if [[ -d "/usr/local/Cellar/apktool" ]]; then
-	apktool_version=$(ls /usr/local/Cellar/apktool | head -n 1)
-	apktool_bin="/usr/local/Cellar/apktool/${apktool_version}/libexec/apktool_${apktool_version}.jar"
-	alias apktool="java -jar ${apktool_bin}"
+    apktool_version=$(ls /usr/local/Cellar/apktool | head -n 1)
+    apktool_bin="/usr/local/Cellar/apktool/${apktool_version}/libexec/apktool_${apktool_version}.jar"
+    alias apktool="java -jar ${apktool_bin}"
 fi
 
 # set java env
 if [[ -d /usr/local/opt/java11 ]]; then
-	export JAVA_HOME="/usr/local/opt/java11"
+    export JAVA_HOME="/usr/local/opt/java11"
 elif [[ -d /usr/local/opt/java ]]; then
-	export JAVA_HOME="/usr/local/opt/java"
+    export JAVA_HOME="/usr/local/opt/java"
 fi
--- scripts/init/init-phar.sh
+++ scripts/init/init-phar.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 ###
- # @Author: Cloudflying
- # @Date: 2022-08-22 23:46:22
- # @LastEditTime: 2023-10-11 10:16:10
- # @LastEditors: Cloudflying
- # @Description: Phar Manager {install update remove}
- # @FilePath: /.boxs/scripts/phar.sh
+# @Author: Cloudflying
+# @Date: 2022-08-22 23:46:22
+# @LastEditTime: 2024-04-17 11:39:24
+# @LastEditors: Cloudflying
+# @Description: Phar Manager {install update remove}
+# @FilePath: /.boxs/scripts/phar.sh
 ###
 
 . ${HOME}/.boxs/scripts/function.sh
@@ -19,86 +19,82 @@ BIN_PATH=${HOME}/.local/boxs/bin
 GIT_HOST="https://ghproxy.com"
 
 # username/repo
-get_github_latest_version()
-{
-	repo=$1
-	url="${GIT_HOST}/${repo}/releases/latest"
-	version=$(curl -sI "${url}" | grep -Ei "${GIT_HOST}/\S+releases/tag/" | awk -F 'releases/tag/' '{print $2}')
-	if [[ -n "${version}" ]]; then
-		echo "${version}"
-	fi
+get_github_latest_version() {
+    repo=$1
+    url="${GIT_HOST}/${repo}/releases/latest"
+    version=$(curl -sI "${url}" | grep -Ei "${GIT_HOST}/\S+releases/tag/" | awk -F 'releases/tag/' '{print $2}')
+    if [[ -n "${version}" ]]; then
+        echo "${version}"
+    fi
 }
 
 # 从远程获取文件
 # _fetch url save
-function _fetch()
-{
-	if [[ -n "${2}" ]]; then
-		_WGET_OUTPUT="-O $2"
-		_AXEL_OUTPUT="-o $2"
-	fi
+function _fetch() {
+    if [[ -n "${2}" ]]; then
+        _WGET_OUTPUT="-O $2"
+        _AXEL_OUTPUT="-o $2"
+    fi
 
-	if [[ -n "$(command -v axel)" ]]; then
-		axel -k -v -a -n 4 $1 $_AXEL_OUTPUT
-	elif [[ -n "$(command -v wget)" ]]; then
-		wget -c --no-check-certificate $1 $_WGET_OUTPUT
-	fi
+    if [[ -n "$(command -v axel)" ]]; then
+        axel -k -v -a -n 4 $1 $_AXEL_OUTPUT
+    elif [[ -n "$(command -v wget)" ]]; then
+        wget -c --no-check-certificate $1 $_WGET_OUTPUT
+    fi
 }
 
-add_bin()
-{
-	mkdir -p ${HOME}/.bin
-	cd ${HOME}/.bin
-	if [[ ! -f "$2" ]]; then
-		_fetch $1 $2
-    	chmod +x $2
-	else
-		echo $2 "has been downloaded"
-	fi
+add_bin() {
+    mkdir -p ${HOME}/.bin
+    cd ${HOME}/.bin
+    if [[ ! -f "$2" ]]; then
+        _fetch $1 $2
+        chmod +x $2
+    else
+        echo $2 "has been downloaded"
+    fi
 }
 
-fetch_latest_version_phar()
-{
-	repo=$1
-	filename=$2
-	save=$3
+fetch_latest_version_phar() {
+    repo=$1
+    filename=$2
+    save=$3
 
-	if [[ -f "${HOME}/.bin/$save" ]]; then
-		echo "${save}" "has been downloaded"
-	else
-		ver=$(get_github_latest_version $1 | tr -d '\n\r')
-		ver=$(echo -e "${ver}" | sed "s# ##g")
-		[ -z "${repo}" ] && echo "repo param null" && exit 1
-		[ -z "${filename}" ] && echo "filename param null" && exit 1
-		[ -z "${save}" ] && echo "save param null" && exit 1
-		[ -z "${ver}" ] && echo "ver is null, can't fetch latest version" && exit 1
-		fullUrl="${GIT_HOST}/${repo}/releases/download/${ver}/${filename}"
-		echo "Fetch ${save} ${ver}"
-		add_bin "${fullUrl}" "${save}"
-	fi
+    if [[ -f "${HOME}/.bin/$save" ]]; then
+        echo "${save}" "has been downloaded"
+    else
+        ver=$(get_github_latest_version $1 | tr -d '\n\r')
+        ver=$(echo -e "${ver}" | sed "s# ##g")
+        [ -z "${repo}" ] && echo "repo param null" && exit 1
+        [ -z "${filename}" ] && echo "filename param null" && exit 1
+        [ -z "${save}" ] && echo "save param null" && exit 1
+        [ -z "${ver}" ] && echo "ver is null, can't fetch latest version" && exit 1
+        fullUrl="${GIT_HOST}/${repo}/releases/download/${ver}/${filename}"
+        echo "Fetch ${save} ${ver}"
+        add_bin "${fullUrl}" "${save}"
+    fi
 }
 
-add_bin https://phpmd.org/static/latest/phpmd.phar 					${BIN_PATH}/phpmd
-add_bin https://phar.phpunit.de/phpcpd.phar 						${BIN_PATH}/phpcpd
-add_bin https://phar.io/releases/phive.phar 						${BIN_PATH}/phive
+add_bin https://phpmd.org/static/latest/phpmd.phar ${BIN_PATH}/phpmd
+add_bin https://phar.phpunit.de/phpcpd.phar ${BIN_PATH}/phpcpd
+add_bin https://phar.io/releases/phive.phar ${BIN_PATH}/phive
 # add_bin https://phar.phpunit.de/phpunit.phar 						${BIN_PATH}/phpunit
-add_bin https://phar.phpbu.de/phpbu.phar 							${BIN_PATH}/phpbu
-add_bin https://www.phing.info/get/phing-latest.phar 				${BIN_PATH}/phing
-add_bin https://phpdoc.org/phpDocumentor.phar 						${BIN_PATH}/phpDocumentor
-add_bin https://doctum.long-term.support/releases/dev/doctum.phar 	${BIN_PATH}/doctum
-add_bin https://phar.phpunit.de/phploc.phar 						${BIN_PATH}/phploc
-add_bin https://www.laravel-enlightn.com/security-checker.phar 		${BIN_PATH}/security-checker
+add_bin https://phar.phpbu.de/phpbu.phar ${BIN_PATH}/phpbu
+add_bin https://www.phing.info/get/phing-latest.phar ${BIN_PATH}/phing
+add_bin https://phpdoc.org/phpDocumentor.phar ${BIN_PATH}/phpDocumentor
+add_bin https://doctum.long-term.support/releases/dev/doctum.phar ${BIN_PATH}/doctum
+add_bin https://phar.phpunit.de/phploc.phar ${BIN_PATH}/phploc
+add_bin https://www.laravel-enlightn.com/security-checker.phar ${BIN_PATH}/security-checker
 
 # fetch_github_latest_release FriendsOfPHP/PHP-CS-Fixer php-cs-fixer.phar ${BIN_PATH}/php-cs-fixer
 # fetch_github_latest_release phpstan/phpstan 			phpstan.phar 		${BIN_PATH}/phpstan
-fetch_github_latest_release deployphp/deployer 			deployer.phar  		${BIN_PATH}/deployer
-fetch_github_latest_release phpro/grumphp 				grumphp.phar 		${BIN_PATH}/grumphp
+fetch_github_latest_release deployphp/deployer deployer.phar ${BIN_PATH}/deployer
+fetch_github_latest_release phpro/grumphp grumphp.phar ${BIN_PATH}/grumphp
 # fetch_github_latest_release vimeo/psalm 				psalm.phar 			${BIN_PATH}/psalm
 # fetch_github_latest_release phan/phan 					phan.phar 			${BIN_PATH}/phan
 # fetch_github_latest_release squizlabs/PHP_CodeSniffer 	phpcbf.phar 		${BIN_PATH}/phpcbf
 # fetch_github_latest_release squizlabs/PHP_CodeSniffer 	phpcs.phar 			${BIN_PATH}/phpcs
-fetch_github_latest_release infection/infection 		infection.phar 		${BIN_PATH}/infection
-fetch_github_latest_release qossmic/deptrac 			deptrac.phar 		${BIN_PATH}/deptrac
+fetch_github_latest_release infection/infection infection.phar ${BIN_PATH}/infection
+fetch_github_latest_release qossmic/deptrac deptrac.phar ${BIN_PATH}/deptrac
 
 # Deprecated
 # fetch_latest_version_phar mihaeu/dephpend 0.8.0/dephpend-0.8.0.phar dephpend
@@ -106,3 +102,9 @@ fetch_github_latest_release qossmic/deptrac 			deptrac.phar 		${BIN_PATH}/deptra
 # add_bin https://cs.symfony.com/download/php-cs-fixer-v3.phar php-cs-fixer
 
 chmod +x ${BIN_PATH}/*
+
+# Install Global Composer Package
+if [[ -n "$(command -v composer)" ]]; then
+    composer global require --dev vimeo/psalm
+    composer global require --dev phan/phan
+fi
