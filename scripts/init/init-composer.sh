e98ba2397e398e9d94761dd5f051f9692335dead
imxieke <oss@live.hk>
2024-04-23 00:42:45 +0800

    update init-composer.sh

---
+++ scripts/init/init-composer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2024-04-23 00:41:21
+ # @LastEditTime: 2024-04-23 00:42:28
+ # @LastEditors: Cloudflying
+ # @Description: Composer Global Init
+###
+
+
+composer global require phpunit/phpunit
+composer global require phpstan/phpstan
+composer global require friendsofphp/php-cs-fixer
+composer global require squizlabs/php_codesniffer
+composer global require vimeo/psalm
+composer global require phan/phan
