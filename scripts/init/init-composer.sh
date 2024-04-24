ad147a181e6f86354df18b9b7207b59f6164f3f1
imxieke <oss@live.hk>
2024-04-24 18:04:44 +0800

    update composer and .gitconfig and add init-debian-cli.sh

--- scripts/init/init-composer.sh
+++ scripts/init/init-composer.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 ###
- # @Author: Cloudflying
- # @Date: 2024-04-23 00:41:21
- # @LastEditTime: 2024-04-23 00:42:28
- # @LastEditors: Cloudflying
- # @Description: Composer Global Init
+# @Author: Cloudflying
+# @Date: 2024-04-23 00:41:21
+# @LastEditTime: 2024-04-23 00:42:28
+# @LastEditors: Cloudflying
+# @Description: Composer Global Init
 ###
 
-
-composer global require phpunit/phpunit
-composer global require phpstan/phpstan
-composer global require friendsofphp/php-cs-fixer
-composer global require squizlabs/php_codesniffer
-composer global require vimeo/psalm
-composer global require phan/phan
+composer global require --dev phpunit/phpunit
+composer global require --dev phpstan/phpstan
+composer global require --dev friendsofphp/php-cs-fixer
+composer global require --dev squizlabs/php_codesniffer
+composer global require --dev vimeo/psalm
+composer global require --dev phan/phan
